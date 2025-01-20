4af63724622410f55651612c4e298d18f4b5fa45
ahmedbouachiri <ahmed.bouachiri08@gmail.com>
2025-01-20 22:05:52 +0100

    SaisirAutoEcole

--- src/main/java/fxml/AutoEcoleFxml.java
+++ src/main/java/fxml/AutoEcoleFxml.java
@@ -1,4 +1,29 @@
 package fxml;
 
+import entities.AutoEcole;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class AutoEcoleFxml {
+    @FXML
+    private TextField nomAutoEcole;
+    @FXML
+    private TextField adresseAutoEcole;
+    @FXML
+    private TextField telAutoEcole;
+    @FXML
+    private TextField emailAutoEcole;
+
+    @FXML
+    public static AutoEcole saisirAutoEcole() {
+        String nomAutoEcole =nomAutoEcole.getText();
+        String adresseAutoEcole= adresseAutoEcole.getText();
+        int telAutoEcole=telAutoEcole.getText();
+        String emailAutoEcole=emailAutoEcole.getText();
+
+        return new AutoEcole(nomAutoEcole,adresseAutoEcole,telAutoEcole,emailAutoEcole);
+    }
+
+
+
 }
